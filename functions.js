9ddcf54ff85b277bcfcfc3cef57c4f20e35350c6
Brackets-Coder <titustedder@icloud.com>
2025-08-21 18:31:23 -0400

    Add MIDI Visualizer

---
+++ functions.js
@@ -0,0 +1,140 @@
+import * as THREE from 'three';
+
+let WhiteKeyWidth  = 0.025;
+let WhiteKeyDepth  = 0.15;
+let WhiteKeyHeight = 0.025;
+let BlackKeyWidth  = 0.0137;
+let BlackKeyDepth  = 0.09;
+let BlackKeyHeight = 0.01;
+
+export function isBlackKey(MIDINote) {
+  const NoteMod = MIDINote % 12;
+  return (NoteMod === 1 || NoteMod === 3 || NoteMod === 6 || NoteMod === 8 || NoteMod === 10);
+}
+
+export function noteTransform(MIDINote) {
+
+  const TotalWhiteKeys = 52;
+  const TotalWhiteKeyWidth = WhiteKeyWidth * TotalWhiteKeys;
+  const StartX = (-TotalWhiteKeyWidth / 2) + (WhiteKeyWidth / 2);
+  const BaseY = 0;
+  const BaseZ = 0;
+
+  const NoteMod = MIDINote % 12;
+  const isBlackKey = (NoteMod === 1 || NoteMod === 3 || NoteMod === 6 || NoteMod === 8 || NoteMod === 10);
+
+  let WhiteKeyCount = 0;
+  for (let i = 21; i < MIDINote; i++) {
+    const Mod = i % 12;
+    if (!(Mod === 1 || Mod === 3 || Mod === 6 || Mod === 8 || Mod === 10)) {
+      WhiteKeyCount++;
+    }
+  }
+
+  if (!isBlackKey) {
+    const X = StartX + WhiteKeyCount * WhiteKeyWidth;
+    return {
+      position: new THREE.Vector3(X, BaseY, BaseZ),
+      scale: new THREE.Vector3(WhiteKeyWidth, WhiteKeyDepth, WhiteKeyHeight),
+      quaternion: new THREE.Quaternion() // identity
+    };
+  } else {
+    let OffsetWeight = 0;
+    switch (NoteMod) {
+      case 1:  OffsetWeight = -0.175; break; // C#
+      case 3:  OffsetWeight =  0.175; break; // D#
+      case 6:  OffsetWeight = -0.175; break; // F#
+      case 8:  OffsetWeight =  0.0;   break; // G#
+      case 10: OffsetWeight =  0.175; break; // A#
+    }
+
+    const X = StartX + (WhiteKeyCount - 1) * WhiteKeyWidth + (WhiteKeyWidth * 0.5) + (OffsetWeight * BlackKeyWidth);
+    const Y = (BaseY + (WhiteKeyDepth - BlackKeyDepth) / 2);
+    const Z = BaseZ + ((WhiteKeyHeight - BlackKeyHeight) / 2) + BlackKeyHeight;
+
+    return {
+      position: new THREE.Vector3(X, Y, Z),
+      scale: new THREE.Vector3(BlackKeyWidth, BlackKeyDepth, BlackKeyHeight),
+    };
+  }
+}
+
+let keys = [];
+export function generatePianoKeyboard(scene) {
+  for (let midiNote = 21; midiNote <= 108; midiNote++) {
+    const { position, scale } = noteTransform(midiNote);
+    const isBlackKey = [1, 3, 6, 8, 10].includes(midiNote % 12);
+    const color = isBlackKey ? 0x000000 : 0xffffff;
+
+    const geometry = new THREE.BoxGeometry(scale.x, scale.y, scale.z);
+    const material = new THREE.MeshStandardMaterial({ color, emissive: "rgb(0, 128, 128)", emissiveIntensity: 0 });
+    const key = new THREE.Mesh(geometry, material);
+
+    key.position.copy(position);
+    scene.add(key);
+    keys.push(key);
+  }
+}
+
+export function setKeyDimensions(scene, whiteWidth, whiteDepth, whiteHeight, blackWidth, blackDepth, blackHeight) {
+  WhiteKeyWidth  = whiteWidth;
+  WhiteKeyDepth  = whiteDepth;
+  WhiteKeyHeight = whiteHeight;
+  BlackKeyWidth  = blackWidth;
+  BlackKeyDepth  = blackDepth;
+  BlackKeyHeight = blackHeight;
+  
+  generatePianoKeyboard(scene)
+}
+
+
+export function lightKeyOn(midiNote) {
+  const key = keys[midiNote - 21];
+  if (key) {
+    key.material.color.set(0x0000);
+    key.material.emissiveIntensity = 2;
+  }
+}
+
+export function lightKeyOff(midiNote) {
+  const key = keys[midiNote - 21];
+  if (key) {
+    key.material.color.set(isBlackKey(midiNote) ? 0x000000 : 0xffffff);
+    key.material.emissiveIntensity = 0;
+  }
+}
+
+export function createNote(MIDI) {
+  const { position, scale } = noteTransform(MIDI);
+  const geometry = new THREE.PlaneGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({ color: 0x000000, emissive: "rgb(0, 128, 128)", side: THREE.DoubleSide, transparent: true, emissiveIntensity: 2 });
+  const note = new THREE.Mesh(geometry, material);
+  note.position.x = position.x;
+  note.scale.x = scale.x;
+  note.scale.y = 0;
+  note.scale.z = 1;
+  setNoteRoundness(0, note);
+  note.on = true;
+  note.midi = MIDI;
+  return note;
+}
+
+// TODO: This needs to be improved for performance
+export function setNoteRoundness(roundness, note) {
+  const c = document.createElement("canvas");
+  c.width = 1024 * note.scale.x;
+  c.height = 1024 * note.scale.y;
+  const ctx = c.getContext("2d");
+
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, c.width, c.height);
+
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  ctx.roundRect(0, 0, c.width, c.height, (roundness / 100) * (Math.min(c.width, c.height) / 2));
+  ctx.fill();
+
+  note.material.alphaMap = new THREE.CanvasTexture(c);
+
+  note.material.needsUpdate = true;
+}
---
+++ main.js
@@ -0,0 +1,254 @@
+import * as THREE from 'three';
+import * as Functions from './functions.js';
+import * as Shaders from './shaders.js';
+import { PostProcessing, WebGPURenderer } from 'three/webgpu';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import Stats from 'three/addons/libs/stats.module.js';
+// webgl postprocessing
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js"
+// webgpu postprocessing
+import { pass } from 'three/tsl'
+import { bloom } from 'three/addons/tsl/display/BloomNode.js';
+
+"use strict";
+
+//* =-=-=-=-=-=| MIDI SETUP |=-=-=-=-=-=
+
+if (!navigator.requestMIDIAccess) {
+
+  const h = document.createElement("h1");
+  h.innerText = "MIDI Access Denied";
+  const p = document.createElement("p");
+  p.innerText = "Your browser does not support the Web MIDI API. Please try a different browser or check your browser settings to enable MIDI access.";
+  document.getElementById("menu").appendChild(h);
+  document.getElementById("menu").appendChild(p);
+
+  const button = document.createElement("button");
+  button.innerText = "Continue Anyway";
+  button.setAttribute("class", "btn btn-lg btn-primary");
+  button.setAttribute("id", "start");
+  document.getElementById("menu").appendChild(button);
+
+} else {
+
+  const h = document.createElement("h1");
+  h.innerText = "Hello";
+  document.getElementById("menu").appendChild(h);
+
+  const button = document.createElement("button");
+  button.innerText = "Start";
+  button.setAttribute("class", "btn btn-lg btn-primary");
+  button.setAttribute("id", "start");
+  document.getElementById("menu").appendChild(button);
+
+  document.getElementById("start").addEventListener("click", () => {
+    navigator.requestMIDIAccess().then((midiAccess) => {
+      const inputs = midiAccess.inputs.values();
+      for (let input of inputs) {
+        console.log("hello")
+        input.onmidimessage = (message) => {
+          const [status, note, velocity] = message.data;
+
+          if (status === 144 && velocity > 0) { // Note On
+            const event = new CustomEvent("noteon", { detail: { note, velocity } });
+            document.dispatchEvent(event);
+
+          } else if (status === 128 || (status === 144 && velocity === 0)) { // Note Off
+            const event = new CustomEvent("noteoff", { detail: { note } });
+            document.dispatchEvent(event);
+          }
+
+        };
+      }
+    }).catch((err) => {
+      console.error("Failed to get MIDI access:", err);
+    });
+  });
+}
+
+//* =-=-=-=-=-=| RENDER SETUP |=-=-=-=-=-=
+ 
+if ( WebGL.isWebGL2Available() ) {
+
+  const canvas = document.querySelector('#c');
+
+  let renderer;
+  if (navigator.gpu) {
+    renderer = new WebGPURenderer({ canvas, antialias: true });
+  } else {
+    renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  }
+
+  const stats = new Stats();
+  document.body.append( stats.dom );
+  stats.dom.style.zIndex = 1;
+
+  //* =-=-=-=-=-=| SCENE SETUP |=-=-=-=-=-=
+  
+  const camera = new THREE.PerspectiveCamera( 75, 2, 0.001, 999 );
+  camera.position.z = 0.6;
+  const controls = new OrbitControls( camera, renderer.domElement );
+  controls.enablePan = false;
+  controls.enableZoom = false;
+  controls.enableRotate = false;
+  controls.update();
+
+  // * start button
+  document.getElementById("start").addEventListener("click", () => {
+    document.getElementsByClassName("ui-container")[0].style.display = "none";
+    controls.enablePan = true;
+    controls.enableZoom = true;
+    controls.enableRotate = true;
+  });
+  
+  const scene = new THREE.Scene();
+  const AmbientLight = new THREE.AmbientLight( 0xefefef ); // soft white light
+  scene.add( AmbientLight );
+
+  //* =-=-=-=-=-=| POSTPROCESS SETUP |=-=-=-=-=-=
+
+  let composer;
+  let postProcessing
+
+  if (navigator.gpu) {
+    postProcessing = new PostProcessing( renderer );
+
+    const scenePass = pass( scene, camera );
+    const scenePassColor = scenePass.getTextureNode( 'output' );
+    const bloomPass = bloom( scenePassColor );
+    bloomPass.strength.value = 1.5;
+    bloomPass.radius.value = 0.1;
+    bloomPass.threshold.value = 0.3;
+
+    postProcessing.outputNode = scenePassColor.add( bloomPass );
+  } else {
+    composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+  
+    const bloomParams = {
+        strength: 1.5,   // intensity
+        radius: 0.1,       // spread
+        threshold: 0.3 // brightness cutoff
+    };
+    const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), bloomParams.strength, bloomParams.radius, bloomParams.threshold);
+    composer.addPass(bloomPass);
+  }
+
+  //* =-=-=-=-=-=| RESIZER |=-=-=-=-=-=
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const pixelRatio = window.devicePixelRatio;
+    const width  = Math.floor( canvas.clientWidth  * pixelRatio );
+    const height = Math.floor( canvas.clientHeight * pixelRatio );
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+      if (!navigator.gpu) composer.setSize(width, height);
+    }
+    return needResize;
+  }
+
+  //* =-=-=-=-=-=| INPUT SETUP |=-=-=-=-=-=
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === "0") {
+      camera.position.set(0, 0, 5); // Reset camera position
+      camera.lookAt(0, 0, 0); // Ensure the camera is looking at the origin
+      controls.reset(); // Reset OrbitControls to default state
+    }
+  });
+
+  //* =-=-=-=-=-=| GEOMETRY SETUP |=-=-=-=-=-=
+
+  const activeNotes = [];
+  const offNotes = [];
+  
+  // generate the piano keyboard
+  Functions.generatePianoKeyboard(scene);
+
+  // generate the laser
+  const geometry = new THREE.BoxGeometry((0.025 * 52) - 0.001, 0.025 / 2, 0.024, 100, 1, 1);
+  const laser = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({ color: 0x000000, emissive: "rgb(0, 128, 128)", emissiveIntensity: 2 }));
+  laser.position.set(0, (0.15 / 2), 0);
+  scene.add(laser);
+
+  //* =-=-=-=-=-=| GAME LOOP |=-=-=-=-=-=
+
+  document.addEventListener("noteon", (e) => {
+    const { note, velocity } = e.detail;
+
+    Functions.lightKeyOn(note);
+  
+    const newnote = Functions.createNote(note);
+    activeNotes.push(newnote);
+    scene.add(newnote);
+  });
+
+  document.addEventListener("noteoff", (e) => {
+    const { note } = e.detail;
+
+    Functions.lightKeyOff(note);
+
+    const idx = activeNotes.findIndex(n => n.midi === note);
+    if (idx !== -1) {
+      const note = activeNotes[idx];
+      note.on = false;
+      activeNotes.splice(idx, 1);
+      offNotes.push(note);
+    }
+  });
+
+  function tick(time, deltaTime) {
+    activeNotes.forEach(n => {
+      n.scale.y += deltaTime / 5;
+      n.position.y = (n.scale.y / 2) + (0.15 / 3);
+      Functions.setNoteRoundness(50, n);
+    });
+  
+    offNotes.forEach((n, i) => {
+      n.position.y += deltaTime / 5;
+      const bottomY = n.position.y - n.scale.y / 2;
+      if (bottomY > (0.15 * 10)) {
+        scene.remove(n);
+        offNotes.splice(i, 1);
+      }
+    });
+  }
+
+  //* =-=-=-=-=-=| RENDER LOOP |=-=-=-=-=-=
+
+  let prevTime = 0;
+
+  function render(time) {
+    time *= 0.001;
+    const deltaTime = time - prevTime;
+    prevTime = time;
+  
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    
+    tick(time, deltaTime);
+
+    navigator.gpu ? postProcessing.renderAsync() : renderer.render(scene, camera);
+
+    if (!navigator.gpu) composer.render();
+    
+    stats.update();
+  
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+  
+} else {
+  const warning = WebGL.getWebGL2ErrorMessage();
+  document.body.appendChild( warning );
+}
